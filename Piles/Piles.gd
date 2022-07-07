1849612b9f315467d57c1733908ac25f11af2a3c
lieu <lieuzhenghong@gmail.com>
2022-07-07 23:44:16 +0800

    LGTM

--- Piles/Piles.gd
+++ Piles/Piles.gd
@@ -1,5 +1,10 @@
 class_name Piles extends Node
 
+<<<<<<< Updated upstream
+=======
+signal trigger_resolved(type, actor, target)
+
+>>>>>>> Stashed changes
 export(NodePath) var deck_path
 export(NodePath) var hand_path
 export(NodePath) var discard_pile_path
@@ -10,7 +15,11 @@ onready var discard_pile = get_node(discard_pile_path)
 onready var globals = "/root/Globals"
 
 func _ready():
+<<<<<<< Updated upstream
 	pass
+=======
+	Stack.connect("trigger_popped", self, "_on_Trigger_popped")
+>>>>>>> Stashed changes
 
 func load_card():
 	var loaded = deck.load()
@@ -20,5 +29,15 @@ func load_card():
 	hand.add(loaded)
 	print("Deck:" + deck.str())
 
+<<<<<<< Updated upstream
 func _on_Button_pressed():
 	load_card()
+=======
+func _on_Trigger_popped(type, actor, target):
+	print("Trigger of type %s from actor %s targeting %s popped!" % 
+	[type, actor, target])
+	if type == "Load" and target == relevant_player:
+		var loaded = load_card()
+		if loaded != Globals.DECKPILE_EMPTY:
+			emit_signal("trigger_resolved", "load", relevant_player, relevant_player)
+>>>>>>> Stashed changes
